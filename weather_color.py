6f84b5e54869d74abc78289e2f297796372632df
Matt Jarrett <matjar12@yahoo.com>
2016-05-02 20:37:27 -0400

    Add instructables URL to comment section
    Add instructables URL to comment section

--- weather_color.py
+++ weather_color.py
@@ -10,10 +10,10 @@
 # The apiboot.txt and weather_color.py files are intended to reside at /home/pi/weather_color directory and to be launched at 
 # startup by editing crontab with the instruction @reboot sudo python3 /home/pi/weather_color/weather_color.py.
 # 
-# A tutorial for the complete project can be found at xxxx. The basic hardware and software setup can be found at
-# https://learn.adafruit.com/neopixels-on-raspberry-pi. The NeoPixel library for the Raspberry Pi (rpi_ws281x library) can be 
-# found at https://github.com/jgarff. The weather data and API are provided by Weather Underground, LLC (WUL). An API key can 
-# be obtained at www.wunderground.com/weather/api.
+# A tutorial for the complete project can be found at http://www.instructables.com/id/LED-Weather-Forecast. The basic hardware 
+# and software setup can be found at https://learn.adafruit.com/neopixels-on-raspberry-pi. The NeoPixel library for the Raspberry 
+# Pi (rpi_ws281x library) can be found at https://github.com/jgarff. The weather data and API are provided by Weather Underground, 
+# LLC (WUL). An API key can be obtained at www.wunderground.com/weather/api.
 
 import time
 import json
